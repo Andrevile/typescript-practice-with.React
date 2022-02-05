a11aea59e4e15e9ec42c17dd9bb12ea7bb461630
abg3000 <abg3000@naver.com>
2022-02-05 16:56:06 +0900

    고차함수 keyof typeof 타이핑

---
+++ 5.가위바위보/client.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import RSP from './RSP';
+// const Hot = hot(NumberBaseball);
+ReactDOM.render(<RSP></RSP>, document.querySelector('#root'));
